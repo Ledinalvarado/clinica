ec21b5cf99ee44e10070ae9aa0ddbe1ec7052c5f
Ledin Alvarado <ledinalvarado18@gmail.com>
2025-03-18 15:03:41 -0600

    Agregando archivos trabajados para mostrar en el frontend, barra

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -16,7 +16,7 @@
                 <v-divider></v-divider>
 
                 <v-list density="compact" nav>
-                    <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
+                    <v-list-item prepend-icon="mdi-hospital" title="Especialidades" value="myfiles"></v-list-item>
                     <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
                     <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
                 </v-list>
